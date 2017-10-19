644d3c1b119014bbd5416b4bf9e4e36b6d77d751
Pierre Louis Euvrard <pierre-louis.euvrard@etu.u-bordeaux.fr>
2017-10-19 16:44:07 +0200

    ajout allelement

--- k-upplets.py
+++ k-upplets.py
@@ -13,3 +13,26 @@ def upplet(map):
                     if (x1 != x2 or y1 != y2):
                         list= list + [((x1,y1),(x2,y2))]
     return list
+
+def allpointsofthemap(map):
+    length= map.width
+    height = map.height
+    list= []
+    if(length == 0 or height == 0):
+        return list
+    for x in range (length):
+        for y in range (height):
+            list= list + [(x,y)]
+    
+    return list
+
+
+
+    
+def allelements(tab): #
+    elem = tab[0]
+    if len(tab)==1:
+        return elem+", _"
+    del(tab[0])
+    for i in tab:
+        return elem +"("+ allelements(tab)+")" + ", _" +"("+ allelements(tab)+")"
